04b9d50638dea98cc5f4f6d4488b32c3b9691d3f
Bharati <bharati.kawade27@gmail.com>
2026-02-21 12:43:11 +0530

    Main branch modified message

--- app.py
+++ app.py
@@ -1,12 +1,2 @@
-print("Welcome to GitHub!")
-#print("My first repository")
-#print("I updated my code")
-print("Main Project Running")
-# Calculator Feature
-a = 10
-b = 5
-
-print("Addition:", a + b)
-print("Subtraction:", a - b)
-print("Multiplication:", a * b)
-print("Division:", a / b)
+print("Hello from MAIN branch")
+print("Welcome to DevOps Lab")
